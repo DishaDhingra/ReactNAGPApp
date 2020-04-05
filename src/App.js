e012dbb1d156e8707f8632523722874a9f97f06d
unknown <jain20130135@gmail.com>
2020-04-05 23:24:27 +0530

    added routing and changed architecture

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 
-import Blog from './containers/Blog/Blog';
-
+import  NewPost  from '../src/components/newPost/newPost';
+import  FullPost  from '../src/components/fullPost/fullPost';
+import  PostListing  from '../src/components/postsListing/postListing.js';
+import { Route, Switch } from "react-router-dom";
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Blog />
+        <Switch>
+          <Route exact path="/" component={PostListing} />} />
+          <Route exact path="/createPost" component={NewPost} />} />
+        </Switch>
       </div>
     );
   }
