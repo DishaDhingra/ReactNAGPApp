e1376e2eaaed93e5f32d79890e253c7e08170d2b
Shubhra Kushsal <Shubhra.Kushal@netent.com>
2020-04-12 23:27:50 +0530

    inital routing code

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
+import {BrowserRouter} from 'react-router-dom'
 
-import  NewPost  from '../src/components/newPost/newPost';
-import  FullPost  from '../src/components/fullPost/fullPost';
-import  PostListing  from '../src/components/postsListing/postListing.js';
-import { Route, Switch } from "react-router-dom";
+import Blog from './containers/Blog/Blog';
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Switch>
-          <Route exact path="/" component={PostListing} />} />
-          <Route exact path="/createPost" component={NewPost} />} />
-        </Switch>
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Blog />
+        </div>
+      </BrowserRouter>
     );
   }
 }
--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import './post.css';
+import './Post.css';
 
 const post = (props) => (
     <article className="Post" onClick={props.clicked}>
